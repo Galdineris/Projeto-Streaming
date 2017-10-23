304fa04d7c321626d326bf6d556a512fd0ba80d1
Rodrigo Uchoa <rodrigo.costa9620@gmail.com>
2017-10-23 17:10:51 -0200

    Teste buscar filme por código

--- Projeto-Streaming-master/Projeto-Streaming-master/Streaming/logica/test_filme.py
+++ Projeto-Streaming-master/Projeto-Streaming-master/Streaming/logica/test_filme.py
@@ -13,29 +13,38 @@ class TestFilme(unittest.TestCase):
     def test_adicionar_filme(self):
         filme.adicionar_filme("Homem aranha", "Herói", 2001)
         filmes = filme.listar_filmes()
-        self.assertEqual(3, len(filmes))
+        self.assertEqual(1, len(filmes))
         f = filmes[0]
-        self.assertEqual("Homem aranha", f[0])
-        self.assertEqual("Herói", f[1])
-        self.assertEqual(2001, f[2])
+        self.assertEqual("Homem aranha", f[1])
+        self.assertEqual("Herói", f[2])
+        self.assertEqual(2001, f[3])
 
     def test_adicionar_dois_filmes(self):
-        filme.adicionar_filme("Filme x", "ação", 2000)
+        filme.adicionar_filme("Homem aranha", "Herói", 2001)
         filme.adicionar_filme("Filme y", "aventura", 2000)
         filmes = filme.listar_filmes()
         self.assertEqual(2, len(filmes))
 
     def test_buscar_filme(self):
-         filme.adicionar_filme("Filme x", "ação", 2000)
-         filme.adicionar_filme("Filme y", "aventura", 2000)
+        filme.adicionar_filme("Homem aranha", "Herói", 2001)
+        filme.adicionar_filme("Filme y", "aventura", 2000)
 
-         f = filme.buscar_filme_por_genero("aventura")
-         
-         self.assertEqual("Filme y", f[1])
-         self.assertEqual("aventura", f[2])
+        f = filme.buscar_filme(4)
+        self.assertEqual(4, f[0])
+        self.assertEqual("Homem aranha", f[1])
+
+    def test_buscar_filme_por_genero(self):
+        filme.adicionar_filme("Homem aranha", "Herói", 2001)
+        filme.adicionar_filme("Filme y", "aventura", 2000)
+
+        f = filme.buscar_filme_por_genero("Herói")
+        self.assertEqual("Homem aranha", f[1])
+        self.assertEqual("Herói", f[2])
+        
+        
 
     def test_remover_todos_filmes(self):
-         filme.adicionar_filme("Filme x", "ação", 2000)
+         filme.adicionar_filme("Homem aranha", "Herói", 2001)
          filme.adicionar_filme("Filme y", "aventura", 2000)
 
          filme.remover_todos_filmes()
@@ -45,7 +54,7 @@ class TestFilme(unittest.TestCase):
     def test_iniciar_filmes(self):
         filme.iniciar_filmes()
         filmes = filme.listar_filmes()
-        self.assertEqual(2, len(filmes))
+        self.assertEqual(5, len(filmes))
 
 if __name__ == "__main__":
     unittest.main(exit=False)
