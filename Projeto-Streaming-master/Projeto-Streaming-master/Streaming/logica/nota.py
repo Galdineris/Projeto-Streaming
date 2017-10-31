9ac653d2a9ee05eb647087e7e022a47237aa22a6
artursfc <csf.artur@gmail.com>
2017-10-30 22:26:43 -0200

    add modulo nota

--- Projeto-Streaming-master/Projeto-Streaming-master/Streaming/logica/nota.py
+++ Projeto-Streaming-master/Projeto-Streaming-master/Streaming/logica/nota.py
@@ -1,7 +1,25 @@
-from logica import filme
-
-notas = []
+notas = {}
 
 def adicionar_nota(cod_filme, nota):
-    f = filme.buscar_filme(cod_filme)
-    pass
+    cod = str(cod_filme)
+    if cod in notas:
+        n1 = notas[cod]
+        n1 = (nota+n1)/2
+        notas[cod] = n1
+    else:
+        notas[cod] = nota
+
+def buscar_nota(cod_filme):
+    cod = str(cod_filme)
+    if cod in notas:
+        n1 = notas[cod]
+        return n1
+    else:
+        return None
+
+def inicializar_nota():
+    adicionar_nota(0, 5)
+    adicionar_nota(1, 4.75)
+    adicionar_nota(2, 3)
+    
+    
